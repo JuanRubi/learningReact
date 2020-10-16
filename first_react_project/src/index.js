0f1946740e5d5e2b206fa9911deae53ec7be09c6
Juan Rosales <rjuan361@gmail.com>
2020-10-15 17:54:08 -0700

    Added Properties to Book Component
    Added properties to the Book component to allow for different values to be passed.

--- first_react_project/src/index.js
+++ first_react_project/src/index.js
@@ -5,30 +5,59 @@ import ReactDOM from 'react-dom';
 // CSS
 import './index.css';   // Full address to CSS file.
 
+const firstBook = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/41iers+HLSL._SY344_BO1,204,203,200_.jpg",
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald'
+};
+
+const secondBook = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/51WSrYIgO3L._SX329_BO1,204,203,200_.jpg",
+  title: 'Dead Men Do Tell Tales',
+  author: 'William R. Maples'
+};
+
+const thirdBook = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/51NoqhM4r9L._SX407_BO1,204,203,200_.jpg",
+  title: 'Data Structures and Algorithms',
+  author: 'Robert Lafore'
+};
+
 // BookList Component. 
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.image}    // Properties that will be passed.
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.image}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+
+      <Book
+        img={thirdBook.image}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
     </section>
   );
 }
 
 // Book Component.
-const Book = () => {
+const Book = (props) => {
+  // Object destructuring. Can also do this in parameters section.
+  const { img, title, author } = props;
 
-  // NOTE: Works even if variable are declared outside the component.
-  const title = 'The Great Gatsby';
-  const author = 'F. Scott Fitzgerald';
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/41iers+HLSL._SY344_BO1,204,203,200_.jpg" alt=""
-      />
-      <h2>{/* Item passed in must return a value. */ title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={img} alt="" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 }
