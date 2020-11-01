11628bd2097a7ba68dbe217753790bb478e325d9
Juan Rosales <rjuan361@gmail.com>
2020-10-31 19:07:41 -0700

    Add memo function solution to example. Setup useCallback example.
    Used memo function to solve re-rendering issue in example and created setup for useCallback example.

--- advance_react/src/tutorials/initialVersions/memo_useMemo_useCallback/index.js
+++ advance_react/src/tutorials/initialVersions/memo_useMemo_useCallback/index.js
@@ -11,6 +11,11 @@ const url = "https://course-api.netlify.app/api/javascript-store-products";
 const Index = () => {
     const { products } = useFetch(url);
     const [count, setCount] = useState(0);
+    const [cart, setCart] = useState(0);
+
+    const addToCart = () => {
+        setCart(cart + 1);
+    };
 
     return (
         <>
@@ -19,13 +24,15 @@ const Index = () => {
                 Click Me
             </button>
 
-            <BigList products={products} />
+            <h1 style={{ marginTop: "3rem" }}> Cart: {cart}</h1>
+            <BigList products={products} addToCart={addToCart} />
         </>
     );
 
 };
 
-const BigList = ({ products }) => {
+// memo function checks (memoizing) the props value and doesn't trigger re-render if unchanged.
+const BigList = React.memo(({ products, addToCart }) => {
     useEffect(() => {
         console.log("Big list called.");
     });
@@ -34,14 +41,15 @@ const BigList = ({ products }) => {
         <section className="products">
             {products.map((product) => {
                 return (
-                    <SingleProduct key={product.id} {...product}></SingleProduct>
+                    <SingleProduct key={product.id} {...product}
+                        addToCart={addToCart}></SingleProduct>
                 );
             })}
         </section>
     );
-};
+});
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
     let { name, price } = fields;
     price = price / 100;
     const image = fields.image[0].url;
@@ -51,6 +59,7 @@ const SingleProduct = ({ fields }) => {
             <img src={image} alt={name} />
             <h4>{name}</h4>
             <p>${price}</p>
+            <button onClick={addToCart}>Add To Cart</button>
         </article>
     );
 };
