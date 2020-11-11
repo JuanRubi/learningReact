92f4533b4b66ffa1df9cd613f850c9f9c0f0c846
Juan Rosales <rjuan361@gmail.com>
2020-11-10 17:12:23 -0700

    Added files for Reviews Project.
    Added starting files to begin working on creating a reviews section using React.

---
+++ useEffect_conditionRender_projects/reviews/src/App.js
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------------------
+    GOAL: To display user review.
+----------------------------------------------------------------------------------------*/
+
+import './App.css';
+
+// Importing an icon from react-icons library. 
+import { FaGithubSquare } from 'react-icons/fa';
+
+function App() {
+  return (
+    <>
+      <h2>Reviews Project
+        <FaGithubSquare />
+      </h2>
+    </>
+  );
+}
+
+export default App;
