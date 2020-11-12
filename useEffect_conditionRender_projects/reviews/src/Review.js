6a38832a9041707a4b486de0bdf195d76581b4a9
Juan Rosales <rjuan361@gmail.com>
2020-11-12 14:24:44 -0700

    Added ability to display next or previous review.
    App now allows user to cycle through different reviews.

--- useEffect_conditionRender_projects/reviews/src/Review.js
+++ useEffect_conditionRender_projects/reviews/src/Review.js
@@ -5,11 +5,40 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import './App.css';
 
 const Review = () => {
     const [index, setIndex] = useState(0);
     // Pass in index to display different person as index changes.
     const { name, job, image, text } = people[index];
+
+    // Creating array loop around to solve out of bounds issues.
+    const checkIndex = (index) => {
+        if (index > people.length - 1) {
+            return 0;
+        }
+        if (index < 0) {
+            return people.length - 1;
+        }
+
+        return index;
+    };
+
+    // Accessing next review.
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkIndex(newIndex);
+        });
+    };
+
+    // Accessing previous review.
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkIndex(newIndex);
+        });
+    };
 
     return (
         <article className="review">
@@ -25,10 +54,10 @@ const Review = () => {
             <p className="info">{text}</p>
 
             <div className="button_contatiner">
-                <button className="prev_btn">
+                <button className="prev_btn" onClick={prevPerson}>
                     <FaChevronLeft />
                 </button>
-                <button className="next_btn">
+                <button className="next_btn" onClick={nextPerson}>
                     <FaChevronRight />
                 </button>
                 <button className="random_btn">Random</button>
